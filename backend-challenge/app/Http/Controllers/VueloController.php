e64addeb694f81e02acd9f7f710fb50eae059c45
Edison Simbana <ersimbana2@espe.edu.ec>
2022-03-10 14:00:54 -0500

    creación de migración, con sus respectivos atributos para persona y vuelo, creación de los seeders, modelos y controladores respectivos

---
+++ backend-challenge/app/Http/Controllers/VueloController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vuelo;
+use App\Http\Requests\StoreVueloRequest;
+use App\Http\Requests\UpdateVueloRequest;
+
+class VueloController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreVueloRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreVueloRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Vuelo  $vuelo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Vuelo $vuelo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Vuelo  $vuelo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Vuelo $vuelo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateVueloRequest  $request
+     * @param  \App\Models\Vuelo  $vuelo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateVueloRequest $request, Vuelo $vuelo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Vuelo  $vuelo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Vuelo $vuelo)
+    {
+        //
+    }
+}
