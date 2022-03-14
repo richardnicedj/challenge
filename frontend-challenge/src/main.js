74aaa6f698f440c0ab6c70309daccb2e3ce42f24
Edison Simbana <ersimbana2@espe.edu.ec>
2022-03-14 13:55:48 -0500

    cambios proyecto con vite y vue 2

--- frontend-challenge/src/main.js
+++ frontend-challenge/src/main.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
+import Vue from "vue";
+import App from "./App.vue";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+//createApp(App).mount("#app");
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
