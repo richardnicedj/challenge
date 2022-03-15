823e760918f9d019ff2c1e332543e8e49ec6d73b
Edison Simbana <ersimbana2@espe.edu.ec>
2022-03-15 12:28:39 -0500

    configuración del entorno de desarrollo frontend-challenge

--- frontend-challenge/src/App.vue
+++ frontend-challenge/src/App.vue
@@ -1,18 +1,11 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
-
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button class="btn btn-primary">hola</button>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
   </div>
 </template>
 
@@ -23,6 +16,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
