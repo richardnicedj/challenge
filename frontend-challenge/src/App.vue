753694cb168d9b0db7075ada203cd1c6595023f2
Edison Simbana <ersimbana2@espe.edu.ec>
2022-03-16 09:08:51 -0500

    formulario de busqueda

--- frontend-challenge/src/App.vue
+++ frontend-challenge/src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <div id="app">
-    <button class="btn btn-primary">hola</button>
+  <div id="app" class="container">
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
--- frontend-challenge/src/views/HomeView.vue
+++ frontend-challenge/src/views/HomeView.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <form>
+    <div class="row g-4 needs-validation" novalidate>
+      <div class="form-group col-md-3">
+        <label for="origen">Ciudad de origen</label>
+        <select class="form-select" required>
+          <option selected disabled value="">Seleccione la ciudad</option>
+          <option value="1">Quito</option>
+          <option value="2">Guayaquil</option>
+          <option value="3">Cuenca</option>
+        </select>
+      </div>
+      <div class="form-group col-md-3">
+        <label for="destino">Ciudad de destino</label>
+        <select class="form-select" required>
+          <option selected disabled value="">Seleccione la ciudad</option>
+          <option value="1">Quito</option>
+          <option value="2">Guayaquil</option>
+          <option value="3">Cuenca</option>
+        </select>
+      </div>
+
+      <div class="form-group col-md-3">
+        <label for="date-salida">Fecha de salida</label>
+        <input type="date" required />
+      </div>
+      <div class="form-group col-md-3">
+        <label for="date-retorno">Fecha de retorno</label>
+        <input type="date" required />
+      </div>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
